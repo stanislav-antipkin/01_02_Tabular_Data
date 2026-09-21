867a8d240d08881c2a035e9bd9f6e82fa42f001a
stanislav-antipkin <hsestanpav@gmail.com>
2026-09-21 22:10:32 +0300

    (stanislav-antipkin) Update assignment.R
    Code commited. Возможно, я не очень понял, как именно нужно вставлять код, но пока получилось так. Надеюсь, по итогу всё верно

--- assignment.R
+++ assignment.R
@@ -9,16 +9,19 @@ works <- gutenberg_works()
 # (1) Отберите ряды, в которых gutenberg_author_id равен 65 или 410;
 # после этого выберите два столбца: author, title
 my_data <- works |> 
-  # ваш код здесь
+  filter(gutenberg_author_id %in% c(65, 410)) |>
+  select(author, title)
 
 # (2) Используйте функцию separate(), чтобы разделить 
 # столбец с именем и фамилией на два новых: author, name. 
 # Удалите столбец name
-my_data2 <- my_data |>
-  # ваш код здесь
+my_data_noname <- my_data |>
+  separate(author, into = c("author", "name"), sep = ", ") |>
+  select(-name)
 
 # (3) Используйте group_by() и summarise(), чтобы узнать,
 # сколько произведений Шекспира и Марлоу хранится в библиотеке Gutenberg
 # новый столбец должен называться n, не делайте сортировку 
-my_data3 <- my_data2 |>
-  # ваш код здесь
+my_data_n <- my_data_noname |>
+  group_by(author) |>
+  summarise(n = n())
